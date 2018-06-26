204f9fa3acb73031a7a57a919e870d1cd8f163b9
Valerio Ferreira <valerio@outlook.com>
2018-06-26 18:36:09 -0300

    Android - AppCenter Push

--- AppCenter/Xamarin/Android/Notifications/MainActivity.cs
+++ AppCenter/Xamarin/Android/Notifications/MainActivity.cs
@@ -2,6 +2,11 @@
 using Android.Widget;
 using Android.OS;
 using Android.Support.V7.App;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Push;
+using System;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 namespace Notifications
 {
@@ -14,7 +19,57 @@ namespace Notifications
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            PushNotificationSetup();
+            AppCenter.Start("438ad34c-9c3b-46c5-8f42-436713b336dc", typeof(Analytics), typeof(Crashes), typeof(Push));
+
+
+            //System.Guid? installId = AppCenter.GetInstallIdAsync().Result;
+
+            Guid installId;// = null;
+            var idTask = AppCenter.GetInstallIdAsync();
+            idTask.ContinueWith((task) =>
+            {
+                var result = (task as System.Threading.Tasks.Task<Guid?>).Result;
+                if (result.HasValue)
+                {
+                    installId = result.Value;
+                }
+            });
+
+            //CustomProperties properties = new CustomProperties();
+            //properties.Set("usuarioID", 12345);
+            //AppCenter.SetCustomProperties(properties);
+        }
+
+        private void PushNotificationSetup()
+        {
+            Push.SetSenderId("1007038591765");
+
+            if (!AppCenter.Configured)
+            {
+                Push.PushNotificationReceived += (sender, e) =>
+                {
+                    // Add the notification message and title to the message
+                    var summary = $"Push notification received:" +
+                                  $"\n\tNotification title: {e.Title}" +
+                                  $"\n\tMessage: {e.Message}";
+
+                    // If there is custom data associated with the notification,
+                    // print the entries
+                    if (e.CustomData != null)
+                    {
+                        summary += "\n\tCustom data:\n";
+                        foreach (var key in e.CustomData.Keys)
+                        {
+                            summary += $"\t\t{key} : {e.CustomData[key]}\n";
+                        }
+                    }
+
+                    // Send the notification summary to debug output
+                    System.Diagnostics.Debug.WriteLine(summary);
+                };
+            }
         }
     }
 }
-
